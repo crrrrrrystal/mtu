39370b936b678aca9cf4531ab693278b4a1166aa
apprat <2279499274@qqcom>
2021-05-23 17:18:34 +0800

    update

--- src/edit/textarea.js
+++ src/edit/textarea.js
@@ -1,6 +1,6 @@
 import { define } from '../../core.js'
 
-const template = `<style>:host{display:block;min-height:96px;line-height:1.6;border:solid 1px var(--color-border);border-radius:2px}.root{position:relative;min-height:inherit;max-height:inherit}.text{caret-color:rgba(var(--color-accent));width:100%;background:0 0;font-family:inherit;outline:0;resize:none;border:none;color:inherit;padding:8.5px 12px;height:0;font-size:inherit;line-height:inherit;min-height:inherit;max-height:inherit;word-wrap:break-word;word-break:break-all;box-sizing:border-box;overflow:auto;overflow:overlay;scrollbar-width:thin}.text::placeholder{color:var(--color-text-secondary)}.text::selection{background:rgba(var(--color-accent));color:#fff}.text::-webkit-scrollbar{display:none}.soul{position:absolute;left:0;opacity:0;bottom:0;pointer-events:none;min-height:0;max-height:0}</style><div class="root"><textarea class="text" part="textarea"></textarea> <textarea class="text soul" part="textarea"></textarea></div>`
+const template = `<style>:host{display:block;min-height:96px;line-height:1.6;border:solid 1px var(--color-border);border-radius:2px}.root{position:relative;min-height:inherit;max-height:inherit}.text{caret-color:rgba(var(--color-accent));width:100%;background:0 0;font-family:inherit;outline:0;resize:none;border:none;color:inherit;padding:8.5px 12px;height:0;font-size:inherit;line-height:inherit;min-height:inherit;max-height:inherit;word-wrap:break-word;word-break:break-all;box-sizing:border-box;overflow:auto;overflow:overlay;scrollbar-width:thin;display:block}.text::placeholder{color:var(--color-text-secondary)}.text::selection{background:rgba(var(--color-accent));color:#fff}.text::-webkit-scrollbar{display:none}.soul{position:absolute;left:0;opacity:0;top:0;pointer-events:none;min-height:0;max-height:0}</style><div class="root"><textarea class="text" part="textarea"></textarea> <textarea class="text soul" part="textarea"></textarea></div>`
 const props = ['value', 'disabled', 'placeholder']
 const setup = (shadow, node) => {
   const [text, soul] = shadow.querySelectorAll('.text')
@@ -10,6 +10,8 @@ const setup = (shadow, node) => {
   }
   text.addEventListener('input', update)
   text.addEventListener('change', () => node.dispatchEvent(new Event('change')))
+  const obs = new ResizeObserver(update)
+  obs.observe(node, { box: 'border-box' })
   return {
     onConnected: update,
     onAdopte: update,
